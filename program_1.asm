309f57453065ea60afb453582366e275fe8ecbe2
Wes Gilleland <wes.gillealnd@gmail.com>
2015-05-08 15:31:05 -0400

    Lots of changes. The machine code is useless basically now

--- program_1.asm
+++ program_1.asm
@@ -11,54 +11,67 @@
         ; Encode a character
         cond1: ; if(( plain >= 'a' && plain <= 'x') || (plain => 'A' && plain <= 'X'))
             ; ( plain >= 'a' && plain <= 'x')
-            move 0, TF
-            comp_ge plain, 'a'
-            comp_le plain, 'x'
+            compare_ge plain, 'a'
+            compare_le plain, 'x'
             branch_and cond1_true
-            move 0, TF
-            comp_ge plain, 'A'
-            comp_le plain, 'X'
+            compare_ge plain, 'A'
+            compare_le plain, 'X'
             branch_and cond1_true
-            move 0, TF 
-            jump else_L
+            jumpto else_L
             cond1_true:
-                ;add plain, 2, plain			;;;;;;;;;;;;;;;;
-                add plain, 2, GP4
-                move GP4, plain
-                jump end_proc
+                add plain, 2
+                jumpto end_proc
         else_L:
             cond2:
-                comp_eq plain, 'y'
-                comp_eq plain, 'z'
-                comp_eq plain, 'Y'
-                comp_eq plain, 'Z'
-                branch_or cond2_else
-                jump end_proc
+                compare_eq plain, 'y'
+                compare_eq plain, 'z'
+                compare_eq plain, 'Y'
+                compare_eq plain, 'Z'
+                branch cond2_else
+                jumpto end_proc
             cond2_else:
-                move 0, TF
-                sub plain, 24, GP4
-                move GP4, plain
+                subtract plain, 24
         end_proc:
             move plain, code
-            STK code
+            move code, RV1
+        return
+    END PROC
+
+    getline PROC
+    ;; Implementation of the STL getline() method
+    ;; Take in a pointer to a bytestring and store it into buff
+        move 0, GP1
+        l:
+            ;;while the current character is not null and the current index is less than AR2, store the byte
+            move [AR3+GP1], [AR1+GP1]   ; Move the current character at the GP1 index of AR3 into the same index at AR1
+            add GP1, 1  ; Increment the counter
+            compare_lt GP1, AR2 ; Check if we're within our limit
+            compare_eq 0, [AR3+GP1] ; Or that the current character is not null
+            branch e
+            jumpto l
+        e:
+        return 
     END PROC
 
 .run    ; Main code run here. Use procedures in .code
 	; Print out the prompt
 	move [prompt], AR1
 	call WriteString
-	call GetString
-	POP buff ; Move the inputted value to buff (takes care of the cin.getline() method)
+	call GetString ; GetString returns an offset to the BYTE array it created into RV1
+    move [buff], AR1
+    move 100, AR2
+    move RV1, AR3
+    call getline   ; Move the line into buff
 	; do a loop (not a barrel roll) to encode each character in the buf array by passing it to the encode procedure
-	top_loop:
-	    move 0, GP1
+    move 0, GP1
+    top_loop:
 	    move buff[GP1], plain
-	    call encode
-	    POP buff[GP1]
-	    comp 0, buff[GP1]
-	    branch_eq end_of_loop
-	    add GP1, 1, GP1
-	    jump top_loop
+	    call encode ; Encode has a return value in RV1
+	    move RV1, buff[GP1] ; Move that return value to GP1
+	    compare_eq 0, buff[GP1]
+	    branch end_of_loop
+	    add GP1, 1
+	    jumpto top_loop
 	end_of_loop:
 	; Print out the encoded string
 	move [message], AR1
--- program_2.asm
+++ program_2.asm
@@ -8,19 +8,19 @@ prompt BYTE  "Enter a non-negative integer: ",0 ;prompt for user to enter an int
 message BYTE "Average= ",0
  
 .run
-move 0, GP4
+    move 0, GP4
     top_loop:
         move [prompt], AR1 ; Move the address of the prompt variable to the first argument register for the WriteString method to read
         call WriteString ; WriteString looks at the value of AR1 as a memory location reference and reads the values in sequential memory until it hits a null value signifying the end of a string
-        call GetInt ; GetInt gets an integer value from the user and pushes it to the top of the stack
-        POP GP1 ; Move the top of the stack to GP1
-        move sum, GP2
-        add GP2, GP1, sum ; Add the value to the sum
-        add GP4, 1, GP4
-        comp GP4, 9
-        branch_lt top_loop ; Relative label addressing
-        intdiv sum, 10, average ; Do an integer division operation of average=sum/10
+        call GetInt ; GetInt gets an integer value from the user and returns it to RV1
+        add sum, RV1 ; Add the value to the sum
+        call WriteNewline ;; Assuming built-in method. Writes a newline character to the output
+        add GP4, 1  ; increment the loop counter
+        compare_lt GP4, 9 ; Check for loop bounds
+        branch top_loop ; Relative label addressing
+        div2 sum, 10, average ; Do an integer division operation of average=sum/10
         move [message], AR1 ; 
         call WriteString    ; Assuming built-in method
         move [average], AR1
         call WriteInt       ; Assuming built-in method
+        call WriteNewline   ; Assuming built-in method
